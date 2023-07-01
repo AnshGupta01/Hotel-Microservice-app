bd481fcdbf32283b0447623bfcd62455d65914ab
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-07-02 00:40:19 +0530

    Additions to Service Module and its dependencies

---
+++ ServiceRegistry/src/test/java/com/ansh/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
